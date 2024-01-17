43e83a4c39ec6d214039ee0a602a4d787ffcee09
席媛媛 <xiyuanyuan@baijia.com>
2024-01-17 22:55:52 +0800

    feat:提交

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Tesseract from './components/Tesseract.vue'
+</script>
+
+<template>
+  <Tesseract />
+</template>
+
+<style></style>
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+import {createStyleImportPlugin} from "vite-plugin-style-import"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    createStyleImportPlugin({
+      libs: [
+        {
+          libraryName: 'vant',
+          esModule: true,
+          resolveStyle: (name) => `vant/es/${name}/style/index`,
+        },
+      ],
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
