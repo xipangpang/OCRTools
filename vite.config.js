b26c7faca89ba9f3ff78d181d925615ec8bca7b8
席媛媛 <xiyuanyuan@baijia.com>
2024-01-17 23:20:47 +0800

    feat:提交dist

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import {createStyleImportPlugin} from "vite-plugin-style-import"
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/OCRTools/',
   plugins: [
     vue(),
-    createStyleImportPlugin({
-      libs: [
-        {
-          libraryName: 'vant',
-          esModule: true,
-          resolveStyle: (name) => `vant/es/${name}/style/index`,
-        },
-      ],
-    })
   ],
   resolve: {
     alias: {
